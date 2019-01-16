825f45dcf8e5a897d134f4abcc792f7f59473698
Katie Schmidt <katie_schmidt7@icloud.com>
2019-01-16 09:02:13 -0500

    redo the simulation field and replace the styling using css classes as opposed to placing the flowers one by one in the css using their ids.

--- main.js
+++ main.js
@@ -6,28 +6,57 @@ function calcDegreesFromXAxis(x, y) {
   return (Math.atan2(y, x) * 180) / Math.PI;
 }
 
-//create a flower
-function Flower(xDistance, yDistance, quadrant) {
+function createFlowerObject(e) {
+  const classList = e.target.classList;
+  let xDist;
+  let yDist;
+
+  for (className of classList) {
+    let classNamePair = className.split("__");
+
+    if (classNamePair[0] === "x-coordinate") {
+      if (classNamePair[1] === "center") {
+        xDist = 0;
+      } else {
+        let xCoordPair = classNamePair[1].split("-");
+        if (xCoordPair[0] === "positive") {
+          xDist = xCoordPair[1] * 1;
+        } else {
+          xDist = xCoordPair[1] * -1;
+        }
+      }
+    } else if (classNamePair[0] === "y-coordinate") {
+      if (classNamePair[1] === "center") {
+        yDist = 0;
+      } else {
+        let yCoordPair = classNamePair[1].split("-");
+        if (yCoordPair[0] === "positive") {
+          yDist = yCoordPair[1] * 1;
+        } else {
+          yDist = yCoordPair[1] * -1;
+        }
+      }
+    }
+  }
+  const currentFlower = new Flower(xDist, yDist);
+  return currentFlower;
+}
+
+//Flower Object
+function Flower(xDistance, yDistance) {
   this.xDistance = xDistance;
   this.yDistance = yDistance;
-  this.quadrant = quadrant;
   this.distanceFromHive = getDistanceFromHive(xDistance, yDistance);
   this.angle = calcDegreesFromXAxis(yDistance, xDistance);
 }
 
-const flower_1 = new Flower(-4, 4, 1);
-
-const simulationFieldFlowers = document.getElementsByClassName(
-  "simulation__field-flower"
+const simulationFieldFlowerImgs = document.getElementsByClassName(
+  "simulation-2__flower-img"
 );
 
-function printFlowerInfo(e) {
-  let flowerId = e.target.parentElement.id;
-  console.log(flowerId);
-}
-
-for (var i = 0; i < simulationFieldFlowers.length; i++) {
-  simulationFieldFlowers[i].onclick = function(e) {
-    printFlowerInfo(e);
+for (var i = 0; i < simulationFieldFlowerImgs.length; i++) {
+  simulationFieldFlowerImgs[i].onclick = e => {
+    let newFlower = createFlowerObject(e);
+    console.log(newFlower);
   };
 }
