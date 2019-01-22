cba475dfecb3b60277442b63f2105904384152b5
Katie Schmidt <katie_schmidt7@icloud.com>
2019-01-22 09:03:17 -0500

    add ids to the animating divs create animation toggler that changes the animationPlayState to running or paused on a click of the button

--- animation.js
+++ animation.js
@@ -1,11 +1,25 @@
 const animationToggler = document.getElementById("animation__button-toggle");
+const animationMover = document.getElementById("animation__mover");
+const animationMoverDiv = document.getElementById("animation__mover-div");
+const animationImage = document.getElementById("animation__image");
+const animationTurner = document.getElementById("animation__turner");
 
-function logIt(e) {
-  return console.log(e);
+function toggleAnimations(content) {
+  if (
+    content.style.animationPlayState === "" ||
+    content.style.animationPlayState === "paused"
+  ) {
+    content.style.animationPlayState = "running";
+    content.style.WebKitAnimationPlayState = "running";
+  } else {
+    content.style.animationPlayState = "paused";
+    content.style.WebkitAnimationPlayState = "paused";
+  }
 }
-animationToggler.onclick = e => {
-  console.log("working again");
-  logIt(e);
-};
 
-console.log("working");
+animationToggler.onclick = () => {
+  toggleAnimations(animationMover);
+  toggleAnimations(animationMoverDiv);
+  toggleAnimations(animationImage);
+  toggleAnimations(animationTurner);
+};
